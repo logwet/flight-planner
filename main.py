5cbd65796b40fa99a889d6857edd1e2e7a340e61
logwet <logwet@users.noreply.github.com>
2023-04-07 20:25:07 +1000

    Parallelize

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import datetime
 import re
 import time
 import urllib.parse
+from datetime import date
 from itertools import permutations
-from typing import Tuple, Dict
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -11,6 +11,8 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+
+from multiprocessing import Pool
 
 
 def td(x) -> datetime.timedelta:
@@ -24,14 +26,6 @@ END_DATE = datetime.date(2024, 1, 21)
 SEARCH_DATE = START_DATE + td(7)
 LATEST_LEAVE_DELAY = 21
 
-driverOptions = Options()
-driverOptions.add_argument('--headless')
-
-driver = webdriver.Chrome(options=driverOptions)
-driver.maximize_window()
-
-price_database: Dict[Tuple[str], Dict[datetime.date, int]] = {}
-
 
 def urlify(s: str) -> str:
     return urllib.parse.quote(s.encode('utf8'))
@@ -42,20 +36,26 @@ def _get_search_url(origin: str, destination: str) -> str:
         f"one way flights for {NUMBER_OF_PEOPLE} people from {origin} to {destination} on {SEARCH_DATE.strftime('%d/%m/%Y')}")
 
 
-def open_page(url):
-    driver.get(url)
+def scrape_price_graph(origin: str, destination: str) -> tuple[tuple[str, str], dict[date, int]]:
+    driverOptions = Options()
+    # driverOptions.add_argument('--headless')
+    driver = webdriver.Remote(
+        command_executor='http://127.0.0.1:4444/wd/hub',
+        desired_capabilities={'browserName': 'chrome', 'javascriptEnabled': True},
+        options=driverOptions)
+    driver.maximize_window()
 
+    driver.get(_get_search_url(origin, destination))
 
-def scrape_price_graph() -> Dict[datetime.date, int]:
     actions = ActionChains(driver)
 
     element = WebDriverWait(driver, 5).until(
         EC.element_to_be_clickable((By.XPATH, "//div[2]/div[2]/div/div/div[2]/button/div")))
     actions.move_to_element(element).click().perform()
 
-    data: Dict[datetime.date, int] = {}
+    data: dict[datetime.date, int] = {}
 
-    def _scrape_graph():
+    def _scrape():
         WebDriverWait(driver, 5).until(
             EC.visibility_of_element_located((By.CSS_SELECTOR, "g[series-id='price graph']")))
         element = WebDriverWait(driver, 5).until(
@@ -87,60 +87,70 @@ def scrape_price_graph() -> Dict[datetime.date, int]:
 
             data[date] = cost
 
-    _scrape_graph()
+    _scrape()
 
     element = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//span/div/div[2]/button/div[3]")))
     actions.move_to_element(element).click().perform()
 
-    _scrape_graph()
+    _scrape()
 
-    return data
+    driver.quit()
 
-
-def get_prices(origin: str, destination: str) -> Dict[datetime.date, int]:
-    if (origin, destination) not in price_database:
-        open_page(_get_search_url(origin, destination))
-        price_database[(origin, destination)] = scrape_price_graph()
-    return price_database[(origin, destination)]
+    return ((origin, destination), data)
 
 
-master_origin_city = "Melbourne"
-destination_cities = {"Colombo": (5, 14), "Kuala Lumpur": (5, 14), "Bangkok": (3, 7)}
-routes = (tuple([master_origin_city] + list(x) + [master_origin_city]) for x in permutations(destination_cities.keys(), 3))
+if __name__ == '__main__':
+    price_database: dict[tuple[str], dict[datetime.date, int]] = {}
 
-route_costs: Dict[Tuple[str], int] = {}
 
-for route in routes:
-    earliest_date = START_DATE
-    total_cost = 0
+    def get_prices(origin: str, destination: str) -> dict[datetime.date, int]:
+        return price_database[(origin, destination)]
 
-    for origin, destination in zip(route, route[1:]):
-        all_costs: Dict[datetime.date, int] = get_prices(origin, destination)
 
-        if origin in destination_cities:
-            latest_date = earliest_date + td(destination_cities[origin][1])
-            earliest_date = earliest_date + td(destination_cities[origin][0])
-        else:
-            latest_date = earliest_date + td(LATEST_LEAVE_DELAY)
+    master_origin_city = "Melbourne"
+    destination_cities = {"Colombo": (7, 14), "Kuala Lumpur": (7, 14), "Bangkok": (3, 7)}
+    routes = (tuple([master_origin_city] + list(x) + [master_origin_city]) for x in
+              permutations(destination_cities.keys(), 3))
 
-        flights_in_date_range = {date: cost for date, cost in all_costs.items() if
-                                 (date >= earliest_date and date <= latest_date)}
+    with Pool(8) as p:
+        results = p.starmap(scrape_price_graph,
+                            set(permutations(list(destination_cities.keys()) + [master_origin_city], 2)))
+        p.close()
+        p.join()
+        for identifier, results in results:
+            price_database[identifier] = results
 
-        cheapest_date = min(flights_in_date_range, key=flights_in_date_range.get)
-        cheapest_cost = flights_in_date_range[cheapest_date]
+    route_costs: dict[tuple[str], int] = {}
 
-        print("Cheapest flight from", origin, "to", destination, "is on", cheapest_date.strftime("%d/%m/%Y"), "for",
-              cheapest_cost, "AUD")
+    for route in routes:
+        earliest_date = START_DATE
+        total_cost = 0
 
-        earliest_date = cheapest_date
-        total_cost += cheapest_cost
+        for origin, destination in zip(route, route[1:]):
+            all_costs: dict[datetime.date, int] = get_prices(origin, destination)
 
-    route_costs[route] = total_cost
-    print("Total cost for", route, "route is", total_cost, "AUD")
+            if origin in destination_cities:
+                latest_date = earliest_date + td(destination_cities[origin][1])
+                earliest_date = earliest_date + td(destination_cities[origin][0])
+            else:
+                latest_date = earliest_date + td(LATEST_LEAVE_DELAY)
 
-driver.quit()
+            flights_in_date_range = {date: cost for date, cost in all_costs.items() if
+                                     (date >= earliest_date and date <= latest_date)}
 
-cheapest_route = min(route_costs, key=route_costs.get)
-cheapest_route_cost = route_costs[cheapest_route]
+            cheapest_date = min(flights_in_date_range, key=flights_in_date_range.get)
+            cheapest_cost = flights_in_date_range[cheapest_date]
 
-print("Cheapest route is", cheapest_route, "for", cheapest_route_cost, "AUD")
+            print("Cheapest flight from", origin, "to", destination, "is on", cheapest_date.strftime("%d/%m/%Y"), "for",
+                  cheapest_cost, "AUD")
+
+            earliest_date = cheapest_date
+            total_cost += cheapest_cost
+
+        route_costs[route] = total_cost
+        print("Total cost for", route, "route is", total_cost, "AUD")
+
+    cheapest_route = min(route_costs, key=route_costs.get)
+    cheapest_route_cost = route_costs[cheapest_route]
+
+    print("Cheapest route is", cheapest_route, "for", cheapest_route_cost, "AUD")
